70c5e7f53ce5dee7703f587f9874ec5527680799
hsr1205 <hsr1205@kdmt.com>
2019-07-11 00:06:48 +0900

    20190711

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Movie from './movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const movies = [
+  {
+    
+    title: "Matrix",
+    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
+
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
+
+  },
+  {
+    title: "OldBoy",
+    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/81WjGytz7HL._SY445_.jpg"
+  }
+
+]
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        {movies.map((movie,index) =>{
+          return <Movie title={movie.title} poster={movie.poster} key={index} />
+        })}
+        
+      </div>  
+    );  
+    
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; //render하는걸 도와주는 모델이야. 리액트를 웹사이트에 올리고싶다->reactDOM . 리액트를 모바일 앱에 올려놓고싶다->reactNative
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
